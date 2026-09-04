6e2ba86820ff4efa6f3ed48133c2fe01257f6d1e
Gaege Root <gaegeroot@MacBook-Pro.local>
2026-09-03 23:17:41 -0700

    chore: remove obsolete environment configurations

--- infra/environments/dev/main.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "festival-platform-terraform-state-bucket"
-    key          = "dev/terraform.tfstate"
-    region       = "us-east-1"
-    use_lockfile = true
-  }
-}
